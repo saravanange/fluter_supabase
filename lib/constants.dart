c283d500e53ba0e8609a687242a0bde60bc0f0b8
saravanange <saravanangengachalam@gmail.com>
2024-02-04 15:15:24 -0500

    feat: added constant file for env values

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+// lib/constants.dart
+//applicationId "com.example.fluter_supabase"
+
+class Constants {
+  // static const String supabaseApiURL =
+  //     'https://eimaaogbqdwjsyjodsvs.supabase.co';
+  // static const String supabaseApiKey =
+  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpbWFhb2dicWR3anN5am9kc3ZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDE5OTA5MCwiZXhwIjoyMDE1Nzc1MDkwfQ.H_-pOohalW01iPt3o01_X86g89G2JkR5XoSyqBQ2YQQ';
+
+  static const String supabaseApiURL =
+      'https://zifrbynvclsxqhtedzqy.supabase.co';
+  static const String supabaseApiKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppZnJieW52Y2xzeHFodGVkenF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEwNDQ5MzIsImV4cCI6MjAxNjYyMDkzMn0.VSFQegvcY4WdKN813YLmPJMnrpaJqb-MD45gYKJe83Q';
+  static const String googleSignInClientId =
+      '478432004528-fke5arc4vqug24uhdkb9jvc8ehgs0gh3.apps.googleusercontent.com';
+  // Add other constants as needed
+  static const String apiBaseURL = 'https://preview.zenhustles.com';
+}
